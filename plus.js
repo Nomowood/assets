7268ed8cea5b1811a8df54f328d689c53c46c237
Nomowood <swk24.kawa@gmail.com>
2026-05-12 15:32:17 +0900

    Update plus.js

--- plus.js
+++ plus.js
@@ -257,9 +257,6 @@ for (let i = 1; i <= typeCount; i++) {
         catSpanHTML += `<a href="/p/${slug}-items-pokopia.html" style="padding:0 0.5rem; text-decoration:none; color:var(--text-st);">${cat}</a>`;
         catHTML += `<li><a href="/p/${slug}-items-pokopia.html">${cat}</a></li>`;
 }
-const catSection = catCount > 0
-    ? `<h2>${name}の分類</h2><ul class="material-list">${catHTML}</ul>`
-    : "";
 
 // 2-3-8. 記述
     el.insertAdjacentHTML("afterend", `
@@ -280,7 +277,7 @@ const catSection = catCount > 0
 
         <h2>${name}の使い道</h2>
         <p>${d.catbody || ""}ポケモンのすみかに設置すると、${catSpanHTML}のアイテムを好きなポケモンが喜びます。</p>
-    ${catCount > 0 ? `<h2>${name}の分類</h2><ul class="material-list">${catHTML}</ul>
+    ${catCount > 0 ? `<h2>${name}の分類</h2><ul class="material-list">${catHTML}</ul>: ""}
     </div>`);
 });
 
